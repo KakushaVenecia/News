ad28bd305fbc2f2a6debe2955b9cdc6e2f593ff6
KakushaVenecia <kakushavenecia@gmail.com>
2022-05-02 13:24:06 +0300

    This is the initial commit of the config file

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
